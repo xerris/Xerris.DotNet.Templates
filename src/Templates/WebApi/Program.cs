8548233bb0b69ea07fe0f839b7be517823ad29b3
Eric Sondergard <4650644+esond@users.noreply.github.com>
2021-12-04 09:24:34 -0600

    WIP: Adjust Web API template to use WebApplicationBuilder

--- src/Templates/WebApi/Program.cs
+++ src/Templates/WebApi/Program.cs
@@ -4,24 +4,37 @@ using Xerris.WebApi1;
 
 // ReSharper disable StringLiteralTypo
 const string xerrisLogo = @"
-              ^EDWXXXX&HT    >7777^     -7777/    ;}z]]ev>.     -777v. _?e+ |777=  |ce- 7777_    ,+le]]z=,   
-              PH+     1HT    .LDDD&l   |XDDDx  _LXDDDDDDDR&Ui   iRDDRzPDDRn SDDDNxXDDR> RDDDs  |UDRDDDDDRDP+ 
+              ^EDWXXXX&HT    >7777^     -7777/    ;}z]]ev>.     -777v. _?e+ |777=  |ce- 7777_    ,+le]]z=,
+              PH+     1HT    .LDDD&l   |XDDDx  _LXDDDDDDDR&Ui   iRDDRzPDDRn SDDDNxXDDR> RDDDs  |UDRDDDDDRDP+
  cEONNNNNWW   GH-     xH4      =WRDRF,l&DRX>  c&DDDV7++}nWDDDF. iRDDDDDGgUl SDDDRR&PgU~ RDDDs ;DDDDe|^>e&RRDt
 rW$.     NW   GHWXXXX&&b.       _ERDDWDDD6,  +DDDD}      -mDDDL iRDDRF_     SDDDDz.     RDDDs |DDDDn=-  ^+++/
-zW#     ,OX .-|||||||,            eDDDDDv    LRDDDDDDDDDDDDDDRO iRDDD/      SDDDN       RDDDs  +gDDDDRDmgns. 
+zW#     ,OX .-|||||||,            eDDDDDv    LRDDDDDDDDDDDDDDRO iRDDD/      SDDDN       RDDDs  +gDDDDRDmgns.
 zWOggggEmEi=SLeeeeeL$+           >NRDDDDP/   oDDD&ezzzzzzzzzzz} iDDDR-      SDDDP       RDDDs    .|7abEWDRDX+
 .,,,,,,..  dC.     z$+          oDDDNSDRR&e  ,NDDDU_    ,nhhhd_ iDDDR-      SDDDP       RDDDs lpppd,    9DDRg
            dT.....:ud|        ,UDDDh. =XDDRp, ,dDRDDNphP&DDDG>  iDDDR-      SDDDP       RDDDs ,gDDRWpTCgDDR&}
-           uuuuuuuxz;        >PmmmI    ,FmmmP^  _e$G&DDDXEn+    +mmmm,      #mmmF       mmmmi   |#Um&DD&Ohe- 
+           uuuuuuuxz;        >PmmmI    ,FmmmP^  _e$G&DDDXEn+    +mmmm,      #mmmF       mmmmi   |#Um&DD&Ohe-
 ";
 // ReSharper restore StringLiteralTypo
 
-var builder = Host.CreateDefaultBuilder(args)
-    .UseSerilog()
-     .ConfigureWebHostDefaults(webBuilder =>
-     {
-         webBuilder.UseStartup<Startup>();
-     });
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((_, logger) => logger.WriteTo.Console());
+
+var startup = new Startup();
+
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+startup.Configure(app);
+
+
+// var builder = Host.CreateDefaultBuilder(args)
+//     .UseSerilog()
+//      .ConfigureWebHostDefaults(webBuilder =>
+//      {
+//          webBuilder.UseStartup<Startup>();
+//      });
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
